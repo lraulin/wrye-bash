59c878ed789a5ad027017e532151bebd28dea69c
MrD <the.ubik@gmail.com>
2017-05-25 20:03:39 +0200

    for block in iter(partial(ins.read, 2097152), ''): EEE crc code
    https://youtu.be/OSGv2VnC0go?t=12m27s

--- Mopy/bash/bolt.py
+++ Mopy/bash/bolt.py
@@ -47,6 +47,8 @@ import traceback
 close_fds = True
 import types
 from binascii import crc32
+from functools import partial
+# Internal
 import bass
 import chardet
 #-- To make commands executed with Popen hidden
@@ -790,13 +792,10 @@ class Path(object):
     @property
     def crc(self):
         """Calculates and returns crc value for self."""
-        size = self.size
         crc = 0L
         with self.open('rb') as ins:
-            insRead = ins.read
-            insTell = ins.tell
-            while insTell() < size:
-                crc = crc32(insRead(512),crc)
+            for block in iter(partial(ins.read, 512), ''):
+                crc = crc32(block,crc)
         return crc & 0xffffffff
 
     #--Path stuff -------------------------------------------------------
